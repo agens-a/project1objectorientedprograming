f2c02a4893a3b7f3b3dfb9eafeead7b6d7e432e2
agens-a <mops2005r@gmail.com>
2025-11-21 10:11:40 +0100

    Add files via upload

---
+++ projekt1POP/src/Readout.java
@@ -0,0 +1,22 @@
+public class Readout implements Comparable<Readout>{
+    private double value;
+    public Readout(double value){ //constructor
+        this.value=value;
+    }
+    public double getValue(){ //standard getter
+        return value;
+    }
+
+    @Override
+    public int compareTo(Readout readout) {
+        return Double.compare(value, readout.getValue());
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%.3f", value);
+    }
+
+}
+
+
